939993748c5f7e4d0d8a1436ac0e19a1645942ed
Eric <ercx1023@gmail.com>
2026-02-25 19:28:52 -0500

    done

--- src/users.ts
+++ src/users.ts
@@ -2,12 +2,28 @@ import type { User } from "./types";
 
 export const apiResponse: unknown = [
   { name: "Tony", age: 23 },
-  { name: "Kevin", age: "24" }, // invalid
+  { name: "Kevin", age: 24 },
   { name: "Jim", age: 25 },
 ];
 
+function isUser(obj: any): obj is User {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    typeof obj.name === "string" &&
+    typeof obj.age === "number"
+  );
+}
+
+function isUserArray(arr: any): arr is User[] {
+  return Array.isArray(arr) && arr.every(isUser);
+}
+
 export function getUsersData(): User[] {
-  return apiResponse as User[]; // intentionally unsafe
+  if (isUserArray(apiResponse)) {
+    return apiResponse;
+  }
+  throw new Error("apiresponse failed type check");
 }
 
 export function formatAges(users: User[]): string[] {
